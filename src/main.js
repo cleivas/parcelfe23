188303459b8cefe475dee8b25f9e5d19e126fa31
cleivas <clara.leivas@gmail.com>
2024-02-16 10:59:03 +0100

    added all

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import sunFlowerImgUrl from './media/sf.jpg'
+
+console.log(sunFlowerImgUrl)
+const img = document.createElement('img');
+document.body.append(img);
+// img.src = './media./sf.jpg'
+img.src = sunFlowerImgUrl;
