a0eb02e0d1605089c77b29068eb44a0ab3b7f7bb
cleivas <clara.leivas@gmail.com>
2024-02-16 11:23:19 +0100

    ready for deployment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import anime from 'animejs';
 import sunFlowerImgUrl from './media/sf.jpg'
 
 console.log(sunFlowerImgUrl)
@@ -5,3 +6,8 @@ const img = document.createElement('img');
 document.body.append(img);
 // img.src = './media./sf.jpg'
 img.src = sunFlowerImgUrl;
+
+anime({
+    targets: img,
+    rotate: 360
+})
